3fa988b22e64f139118589b11c673653fec6e694
Jared Siemen <siemenjm@gmail.com>
2022-10-09 21:59:50 -0400

    Add InstitutionList

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css';
+import InstitutionList from './components/InstitutionList';
 
 function App() {
     return (
-        <h1>Thrift App</h1>
+        <>
+            <h1>Thrift App</h1>
+            <InstitutionList />
+        </>
     );
 }
 
