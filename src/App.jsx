5e8564c96fe7b6342c0bb076a7c2b67fc000f3ca
Jared Siemen <siemenjm@gmail.com>
2022-10-09 22:07:19 -0400

    Add UserList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import InstitutionList from './components/InstitutionList';
+import UserList from './components/UserList';
 
 function App() {
     return (
         <>
             <h1>Thrift App</h1>
+            <UserList />
             <InstitutionList />
         </>
     );
