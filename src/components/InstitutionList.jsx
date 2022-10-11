17c2de21ecccce2b92ec6868d4551156c2e1d807
Jared Siemen <siemenjm@gmail.com>
2022-10-10 20:11:19 -0400

    Move getInstitutions API fetch from InstitutionList to InstitutionPage so PageHeader has access to info

--- src/components/InstitutionList.jsx
+++ src/components/InstitutionList.jsx
@@ -1,24 +1,6 @@
-import React, { useState, useEffect } from 'react';
 import InstitutionCard from './InstitutionCard';
 
-export default function InstitutionList() {
-    const [institutions, setInstitutions] = useState(null);
-
-    async function getInstitutions() {
-        try {
-            const response = await fetch('http://localhost:4000/institutions');
-            const data = await response.json();
-
-            setInstitutions(data);
-        } catch (error) {
-            console.error(error.message);
-        }
-    }
-
-    useEffect(() => {
-        getInstitutions();
-    }, []);
-
+export default function InstitutionList({ institutions }) {
     function loaded() {
         const allInstitutions = institutions.map((institution) => {
             return <InstitutionCard institution={institution} key={institution.ins_id} />;
--- src/pages/InstitutionPage.jsx
+++ src/pages/InstitutionPage.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import InstitutionDetails from '../components/InstitutionDetails';
 import InstitutionList from '../components/InstitutionList';
 import PageHeader from '../components/PageHeader';
 
 export default function InstitutionPage() {
+    const [institutions, setInstitutions] = useState(null);
+
+    async function getInstitutions() {
+        try {
+            const response = await fetch('http://localhost:4000/institutions');
+            const data = await response.json();
+
+            setInstitutions(data);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getInstitutions();
+    }, []);
+
     return (
         <>
-            <PageHeader page={'Institution'}/>
-            <InstitutionList />
+            <PageHeader page={'Institution'} institutions={institutions}/>
+            <InstitutionList institutions={institutions}/>
             <InstitutionDetails />
         </>
     );
