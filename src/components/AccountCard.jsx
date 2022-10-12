33fc05fa8070c3739990131bd4301fa3be2dd00e
Jared Siemen <siemenjm@gmail.com>
2022-10-12 09:32:17 -0400

    Add dollar sign to current_balance

--- src/components/AccountCard.jsx
+++ src/components/AccountCard.jsx
@@ -5,7 +5,7 @@ export default function AccountCard({ account }) {
         <div className="card account-card">
             <img src="" alt={`${account.ins_id} logo`} />
             <h2>{account.name} - {account.ins_id}</h2>
-            <h2>{account.current_balance}</h2>
+            <h2>${account.current_balance}</h2>
         </div>
     );
 }
