93053fcfe8396f431c21fd02ecb6726ebaf08812
Jared Siemen <siemenjm@gmail.com>
2022-10-13 11:21:08 -0400

    Add delete icon and delete route to InstitutionCard

--- src/components/InstitutionCard.jsx
+++ src/components/InstitutionCard.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { FaTrash } from 'react-icons/fa';
 
 export default function InstitutionCard({ institution }) {
+    function handleSubmit() {
+        deleteInstitution();
+    }
+
+    async function deleteInstitution() {
+        try {
+            const options = {
+                method: 'DELETE'
+            };
+
+            const response = await fetch(`http://localhost:4000/institutions/${institution.ins_id}`, options);
+
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
     return (
         <div className="card institution-card">
             <img src="" alt={`${institution.name} logo`} />
             <h2>{institution.name}</h2>
             <h2>${institution.current_balance || "0.00"}</h2>
+            <form onSubmit={handleSubmit} className='delete-form institution-delete-form'>
+                <button type="submit">
+                    <FaTrash />
+                </button>
+            </form>
         </div>
     );
 }
