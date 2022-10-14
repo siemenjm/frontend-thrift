373998fa3dfbfefdbbcfe084a3570bd377e858a2
Jared Siemen <siemenjm@gmail.com>
2022-10-14 10:52:44 -0400

    Move transaction delete to transaction detail

--- src/components/TransactionCard.jsx
+++ src/components/TransactionCard.jsx
@@ -1,26 +1,8 @@
 import React from 'react';
-import { FaTrash } from 'react-icons/fa';
 
 export default function TransactionCard({ transaction, setCurrentTransaction }) {
     function handleClick(e) {
         setCurrentTransaction(transaction);
-    }
-    
-    function handleSubmit() {
-        deleteTransaction();
-    }
-
-    async function deleteTransaction() {
-        try {
-            const options = {
-                method: 'DELETE'
-            };
-
-            const response = await fetch(`http://localhost:4000/transactions/${transaction.trans_id}`, options);
-
-        } catch (error) {
-            console.error(error.message);
-        }
     }
 
     return (
@@ -28,11 +10,6 @@ export default function TransactionCard({ transaction, setCurrentTransaction }) 
             <h2>{transaction.description}</h2>
             <h2>{transaction.category} - {transaction.date}</h2>
             <h2>${transaction.amount}</h2>
-            <form onSubmit={handleSubmit} className='delete-form transaction-delete-form'>
-                <button type="submit">
-                    <FaTrash />
-                </button>
-            </form>
         </div>
     );
 }
--- src/components/TransactionDetails.jsx
+++ src/components/TransactionDetails.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { FaTrash } from 'react-icons/fa';
 import TransactionDetailList from './TransactionDetailList';
 
 export default function TransactionDetails({ transaction }) {
+    function handleSubmit() {
+        deleteTransaction();
+    }
+
+    async function deleteTransaction() {
+        try {
+            const options = {
+                method: 'DELETE'
+            };
+
+            const response = await fetch(`http://localhost:4000/transactions/${transaction.trans_id}`, options);
+
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
     return (
         <div className="details transaction-details">
             <h2 className="details-title">Transaction Details</h2>
+            <form onSubmit={handleSubmit} className='delete-form transaction-delete-form'>
+                <button type="submit">
+                    <FaTrash />
+                </button>
+            </form>
             <TransactionDetailList transaction={transaction}/>
         </div>
     );
