108ebb0bebae5934481d9ef426304e0485889fb2
Jared Siemen <siemenjm@gmail.com>
2022-10-15 13:16:30 -0400

    Populate TransactionCard with institution and account data

--- src/components/TransactionCard.jsx
+++ src/components/TransactionCard.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UrlContext } from '../context/UrlContext';
 
 export default function TransactionCard({ transaction, setCurrentTransaction }) {
+    const { BASE_URL } = useContext(UrlContext);
+    
+    const [institution, setInsitution] = useState(null);
+    const [account, setAccount] = useState(null);
+    
     function handleClick(e) {
         setCurrentTransaction(transaction);
     }
@@ -12,11 +18,51 @@ export default function TransactionCard({ transaction, setCurrentTransaction }) 
         return formattedDate;
     }
 
+    async function getAccount() {
+        try {
+            const accountId = transaction.credited_account_id || transaction.debited_account_id;
+
+            const response = await fetch(`${BASE_URL}/accounts/${accountId}`);
+            const data = await response.json();
+
+            setAccount(data.account);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    async function getInstitution() {
+        try {
+            const response = await fetch(`${BASE_URL}/institutions/${account.ins_id}`);
+            const data = await response.json();
+
+            setInsitution(data.institution);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getAccount();
+    }, []);
+
+    if (!account) {
+        return <h2>Loading data...</h2>
+    }
+    
+    if (!institution) {
+        getInstitution();
+        return <h2>Loading data...</h2>
+    }
+
     return (
         <div onClick={handleClick} className="card transaction-card">
-            <h2>{transaction.description}</h2>
-            <h2>{transaction.category} - {formatDate(transaction.date)}</h2>
-            <h2>${transaction.amount}</h2>
+            <img src={institution.logo} alt={`${institution.name} logo`} />
+            <div className="transaction-details-container">
+                <h4>{transaction.description}</h4>
+                <h4>{transaction.category} - {formatDate(transaction.date)}</h4>
+            </div>
+            <h4>${transaction.amount}</h4>
         </div>
     );
 }
