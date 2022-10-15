e4e76d0532ccbc854738dc43e1b08ca0e229c814
Jared Siemen <siemenjm@gmail.com>
2022-10-15 09:54:14 -0400

    Add sidebar styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
+import '../styles/Sidebar.css';
+import { FaDollarSign, FaHome, FaPeopleArrows, FaPiggyBank, FaUserCog } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
     return (
         <header className="sidebar">
-            <h1>Thrift Sidebar</h1>
+            <div className='sidebar-title'>
+                <h1>Thrift</h1>
+                <FaPiggyBank />
+            </div>
             <nav>
                 <Link to='/'>
+                    <FaHome />
                     <p className="nav-link">Home</p>
                 </Link>
                 <Link to='/institutions'>
+                    <FaPiggyBank />
                     <p className="nav-link">Institutions</p>
                 </Link>
                 <Link to='/accounts'>
+                    <FaDollarSign />
                     <p className="nav-link">Accounts</p>
                 </Link>
                 <Link to='/transactions'>
+                    <FaPeopleArrows />
                     <p className="nav-link">Transactions</p>
                 </Link>
                 <Link to='/users'>
+                    <FaUserCog />
                     <p className="nav-link">User Profile</p>
                 </Link>
             </nav>
