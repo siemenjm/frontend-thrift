220819d096ef115813167843b342bd355dbc791d
Jared Siemen <siemenjm@gmail.com>
2022-10-15 16:58:31 -0400

    Style Details Header

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/Card.css';
+import '../styles/DetailsHeader.css';
 import '../styles/Form.css';
 import '../styles/List.css';
 import '../styles/Main.css';
