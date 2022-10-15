d84810be9c35aa2b040d30dc4dab8e2800aee1b4
Jared Siemen <siemenjm@gmail.com>
2022-10-15 12:59:57 -0400

    Populate AccountCard with institution data and style

--- src/components/AccountCard.jsx
+++ src/components/AccountCard.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UrlContext } from '../context/UrlContext';
 
 export default function AccountCard({ account, isDetail, setCurrentAccount}) {
+    const { BASE_URL } = useContext(UrlContext);
+    
+    const [institution, setInsitution] = useState(null);
+    
     function handleClick(e) {
         if (!isDetail) {
             setCurrentAccount(account);
         }
     }
 
+    async function getInstitution() {
+        try {
+            const response = await fetch(`${BASE_URL}/institutions/${account.ins_id}`);
+            const data = await response.json();
+
+            setInsitution(data.institution);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getInstitution();
+    }, []);
+
+    if (!institution) {
+        return <h2>Loading data...</h2>
+    }
+
     return (
         <div onClick={handleClick} className="card account-card">
-            <img src="" alt={`${account.ins_id} logo`} />
-            <h2>{account.name} - {account.ins_id}</h2>
-            <h2>${account.current_balance}</h2>
+            <img src={institution.logo} alt={`${institution.name} logo`} />
+            <div className="account-details-container">
+                <h4>{account.name}</h4>
+                <p>{institution.name}</p>
+            </div>
+            <h4 className='account-balance'>${account.current_balance}</h4>
         </div>
     );
 }
