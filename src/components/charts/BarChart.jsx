17c3d208327dc912dd7c39faa81d2691a44baf99
Jared Siemen <siemenjm@gmail.com>
2022-10-16 13:14:26 -0400

    Add bar chart displaying current insitution balances

--- src/components/charts/BarChart.jsx
+++ src/components/charts/BarChart.jsx
@@ -9,7 +9,7 @@ import {
     Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-import faker from 'faker';
+// import faker from 'faker';
 
 ChartJS.register(
     CategoryScale,
@@ -20,32 +20,47 @@ ChartJS.register(
     Legend
 );
 
-export const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: 'top',
+export function BarChart({ labels, balanceData }) {
+    const options = {
+        responsive: false,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Your Institution Balances',
+            },
+            legend: {
+                display: false,
+            },
         },
-        title: {
-            display: true,
-            text: 'Chart.js Bar Chart',
+        scales: {
+            x: {
+                grid: {
+                    display: false,
+                },
+            },
         },
-    },
-};
+    };
 
-const labels = ['Ally Bank', 'Chase Bank'];
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Balance',
+                data: balanceData.map((balance, index) => {
+                    return balanceData[index];
+                }),
+                backgroundColor: balanceData.map((balance, index) => {
+                    if (balance > 0) {
+                        return 'rgba(5, 186, 35, 1.0)';
+                    } else {
+                        return 'rgba(230, 57, 70, 1.0)';
+                    }
+                    // return balanceData[index];
+                }),
+                // backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+        ],
+    };
 
-export const data = {
-    labels,
-    datasets: [
-        {
-            label: 'Balance',
-            data: labels.map(() => faker.datatype.number({ min: -10000, max: 10000})),
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        },
-    ],
-};
-
-export function BarChart() {
     return <Bar options={options} data={data} />;
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { BarChart } from '../components/charts/BarChart';
+import { UrlContext } from '../context/UrlContext';
 
 export default function HomePage() {
+    const { BASE_URL } = useContext(UrlContext);
+
+    const [instiutionData, setInstutionData] = useState(null);
+
+    function getLabels() {
+        const labels = instiutionData.map((institution) => {
+            return institution.name;
+        });
+
+        return labels;
+    }
+
+    function getBalanceData() {
+        const balanceData = instiutionData.map((institution) => {
+            return institution.current_balance;
+        });
+
+        return balanceData;
+    }
+    
+    async function getInstitutionData() {
+        try {
+            const response = await fetch(`${BASE_URL}/institutions`);
+            const data = await response.json();
+
+            setInstutionData(data);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getInstitutionData();
+    }, []);
+
     return (
-        <div>HomePage</div>
+        <>
+            <div>HomePage</div>
+            {instiutionData ? <BarChart labels={getLabels()} balanceData={getBalanceData()} /> : <h2>Loading chart...</h2>}
+        </>
     );
 }
