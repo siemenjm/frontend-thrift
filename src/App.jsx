cf21508165218ffa5801c00a9cb0d343928c3c24
Jared Siemen <siemenjm@gmail.com>
2022-11-23 13:43:57 -0500

    Fix base_url

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { UrlContext } from './context/UrlContext';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 
-const BASE_URL = 'https://backend-thrift.fly.dev/';
+const BASE_URL = 'https://thrift.fly.dev/';
 // const BASE_URL = 'https://backend-thrift.herokuapp.com';
 // const BASE_URL = 'http://localhost:4000';
 
